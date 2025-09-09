e83d26d4dc57d007015c789fa360df38993df09f
boztopuz <boztopuz@gmail.com>
2025-09-09 11:18:27 +0300

    [CSM-90]: Fixed feedbacks

--- main.sh
+++ main.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 set -e
 
+APPDOME_APP_PATH="$AC_APPDOME_IPA_PATH"
+APPDOME_API_KEY="$AC_APPDOME_API_KEY"
+APPDOME_FUSION_SET_ID="$AC_APPDOME_FUSION_SET_ID"
+APPDOME_TEAM_ID="$AC_APPDOME_TEAM_ID"
+APPDOME_IOS_ENTITLEMENT="$AC_APPDOME_IOS_ENTITLEMENTS"
+APPDOME_PROVISION_PROFILE="$AC_APPDOME_PROVISIONING_PROFILES"
+APPDOME_CERTIFICATES="$AC_APPDOME_CERTIFICATES"
+APPDOME_CERTIFICATE_PASS="$AC_APPDOME_CERTIFICATES_PASS"
+APPDOME_SIGN_METHOD="$AC_APPDOME_SIGN_METHOD"
+APPDOME_BUILD_LOGS="$AC_APPDOME_BUILD_LOGS"
+
 download_file() {
-	file_location=$1
-	uri=$(echo $file_location | awk -F "?" '{print $1}')
-	downloaded_file=$(basename $uri)
-	curl -L $file_location --output $downloaded_file && echo $downloaded_file
+	file_location="$1"
+	uri=$(echo "$file_location" | awk -F "?" '{print $1}')
+	downloaded_file=$(basename "$uri")
+	curl -L "$file_location" --output "$downloaded_file" && echo "$downloaded_file"
 }
 
-if [[ $AC_APPDOME_IPA_PATH == *"http"* ]];
+if [[ "$APPDOME_APP_PATH" == *"http"* ]];
 then
-	app_file="../$(download_file $AC_APPDOME_IPA_PATH)"
+	app_file="../$(download_file "$APPDOME_APP_PATH")"
 else
-	app_file="$AC_APPDOME_IPA_PATH"
+	app_file="$APPDOME_APP_PATH"
 fi
 
 certificate_output="$AC_OUTPUT_DIR/certificate.pdf"
-secured_app_output="$AC_OUTPUT_DIR/Appdome_$(basename $app_file)"
+secured_app_output="$AC_OUTPUT_DIR/Appdome_$(basename "$app_file")"
 
 appdome_team_id=""
-if [[ -n $AC_APPDOME_TEAM_ID ]]; then
-	appdome_team_id="--team_id ${AC_APPDOME_TEAM_ID}"
+if [[ -n "$APPDOME_TEAM_ID" ]]; then
+	appdome_team_id="--team_id ${APPDOME_TEAM_ID}"
 fi
 
 entitlement_list=""
-if [[ -n $AC_APPDOME_IOS_ENTITLEMENTS ]]; then
-	entitlement_list="--entitlements ${AC_APPDOME_IOS_ENTITLEMENTS}"
+if [[ -n "$APPDOME_IOS_ENTITLEMENT" ]]; then
+	entitlement_list="--entitlements ${APPDOME_IOS_ENTITLEMENT}"
 fi
 
-provision_profile_list=$(echo "$AC_APPDOME_PROVISIONING_PROFILES" | tr "|" ,)
+provision_profile_list=$(echo "$APPDOME_PROVISION_PROFILE" | tr "|" ,)
 echo "Provision Profile List: $provision_profile_list"
 
 git clone https://github.com/Appdome/appdome-api-bash.git > /dev/null
@@ -36,11 +47,11 @@ cd appdome-api-bash
 
 echo "iOS platform detected"
 
-case $AC_APPDOME_SIGN_METHOD in
+case "$APPDOME_SIGN_METHOD" in
 "Private-Signing")		echo "Private Signing"						
-						./appdome_api.sh --api_key "$AC_APPDOME_API_KEY" \
-							--app $app_file \
-							--fusion_set_id "$AC_APPDOME_FUSION_SET_ID" \
+						./appdome_api.sh --api_key "$APPDOME_API_KEY" \
+							--app "$app_file" \
+							--fusion_set_id "$APPDOME_FUSION_SET_ID" \
 							$appdome_team_id \
 							--private_signing \
 							--provisioning_profiles "$provision_profile_list" \
@@ -50,9 +61,9 @@ case $AC_APPDOME_SIGN_METHOD in
 							
 						;;
 "Auto-Dev-Signing")		echo "Auto Dev Signing"
-						./appdome_api.sh --api_key "$AC_APPDOME_API_KEY" \
-							--app $app_file \
-							--fusion_set_id "$AC_APPDOME_FUSION_SET_ID" \
+						./appdome_api.sh --api_key "$APPDOME_API_KEY" \
+							--app "$app_file" \
+							--fusion_set_id "$APPDOME_FUSION_SET_ID" \
 							$appdome_team_id \
 							--auto_dev_private_signing \
 							--provisioning_profiles "$provision_profile_list" \
@@ -62,15 +73,15 @@ case $AC_APPDOME_SIGN_METHOD in
 							
 						;;
 "On-Appdome")			echo "On Appdome Signing"
-						keystore_file="$AC_APPDOME_CERTIFICATES"
-						keystore_pass="$AC_APPDOME_CERTIFICATES_PASS"
-						./appdome_api.sh --api_key "$AC_APPDOME_API_KEY" \
+						certificate_file="$APPDOME_CERTIFICATES"
+						certificate_pass="$APPDOME_CERTIFICATE_PASS"
+						./appdome_api.sh --api_key "$APPDOME_API_KEY" \
 							--app "$app_file" \
-							--fusion_set_id "$AC_APPDOME_FUSION_SET_ID" \
+							--fusion_set_id "$APPDOME_FUSION_SET_ID" \
 							$appdome_team_id \
 							--sign_on_appdome \
-							--keystore "$keystore_file" \
-							--keystore_pass "$keystore_pass" \
+							--keystore "$certificate_file" \
+							--keystore_pass "$certificate_pass" \
 							--provisioning_profiles "$provision_profile_list" \
 							$entitlement_list \
 							--output "$secured_app_output" \
@@ -80,10 +91,10 @@ case $AC_APPDOME_SIGN_METHOD in
 esac
 
 if [[ "$secured_app_output" == *.sh ]]; then
-	echo "AC_APPDOME_PRIVATE_SIGN_SCRIPT_PATH=$secured_app_output" >> $AC_ENV_FILE_PATH
+	echo "AC_APPDOME_PRIVATE_SIGN_SCRIPT_PATH=$secured_app_output" >> "$AC_ENV_FILE_PATH"
 elif [[ "$secured_app_output" == *.ipa ]]; then
-	echo "AC_APPDOME_SECURED_IPA_PATH=$secured_app_output" >> $AC_ENV_FILE_PATH
+	echo "AC_APPDOME_SECURED_IPA_PATH=$secured_app_output" >> "$AC_ENV_FILE_PATH"
 else
 	echo "Secured app output type is undefined: $secured_app_output"
 fi
-echo "AC_APPDOME_CERTIFICATE_PATH=$certificate_output" >> $AC_ENV_FILE_PATH
+echo "AC_APPDOME_CERTIFICATE_PATH=$certificate_output" >> "$AC_ENV_FILE_PATH"
